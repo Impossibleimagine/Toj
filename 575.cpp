dbe4d803148332212b850af3a4e27aff120f7806
Tr00y2ooo <08005252a@gmail.com>
2021-08-04 12:57:33 +0800

    Create 575.cpp

---
+++ 575.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define LoveRem ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define DEBUG 0
+#define int long long
+using namespace std;
+//string S="ABCDEFGHIJKLMNOPQRSTUVWXYZ",s="abcdefghijklmnopqrstuvwxyz";
+int arr[1000010];
+
+signed main()
+{
+    LoveRem
+    if(DEBUG) cout<<"-----------OUT PUT-----------\n";
+    //freopen("oput.txt","w",stdout);
+    int n,k;
+    cin>>n>>k;
+    vector<int> fr(1000010);
+    int a,b;
+    //for(int i=0;i<k;i++){
+        //cin>>a>>b;
+
+    //}
+    cout<<fr[2];
+}
+
